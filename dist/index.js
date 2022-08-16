f5052ef1c5e219233ee10b2a2112d4e1e19e7f42
Jay <me@aeiko.de>
2022-08-16 21:56:52 +0200

    Update basic types

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,38 @@
 "use strict";
+// Basic Types
 let id = 5;
+let firm = "Cool Company";
+let isPrinted = true;
+let anyDataType = "hello world"; // can be string, number, boolean
+let ids = [1, 2, 3, 4,]; // Array
+let array = [2, "jake"];
+// Tuple
+let person = [1, "jake", true]; // has to be exact order and structure
+// Tuple Array
+let employee; // array can have any combination
+employee = [
+    [1, "mike"],
+    [2, "Jill"],
+    [3, "Kimberly"]
+];
+// Union
+let productId = 22; // "22" can be string or number
+// Enum - enumberated types
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: "Jim"
+};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,49 @@
+// Basic Types
 let id: number = 5
+let firm: string = "Cool Company"
+let isPrinted: boolean = true
+let anyDataType: any = "hello world"  // can be string, number, boolean
+
+let ids: number[] = [1, 2, 3, 4, ]  // Array
+let array: any[] = [2, "jake"]
+
+// Tuple
+let person: [number, string, boolean] = [1, "jake", true]  // has to be exact order and structure
+// Tuple Array
+let employee: [number, string][]  // array can have any combination
+
+employee = [
+    [1, "mike"],
+    [2, "Jill"],
+    [3, "Kimberly"]
+]
+
+// Union
+let productId: string | number = 22  // "22" can be string or number
+
+// Enum - enumberated types
+enum Direction1 {
+    Up = 1,
+    Down,
+    Left,
+    Right
+}
+
+enum Direction2{
+    Up = "Up",
+    Down = "Down" ,
+    Left = "Left",
+    Right = "Right"
+}
+
+// Objects
+type User = {
+    id: number,
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: "Jim"
+}
 
